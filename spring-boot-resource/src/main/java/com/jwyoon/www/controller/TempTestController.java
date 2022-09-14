0c1107cef521d507955904c8ed1f32c17dcbb678
iuh12 <iuh12@DESKTOP-UCS6DE1>
2022-09-14 16:33:43 +0900

    vue spring boot 연동

--- spring-boot-resource/src/main/java/com/jwyoon/www/controller/TempTestController.java
+++ spring-boot-resource/src/main/java/com/jwyoon/www/controller/TempTestController.java
@@ -3,13 +3,13 @@ package com.jwyoon.www.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @Controller()
-@RequestMapping("/test")
+@RequestMapping("/api")
 public class TempTestController {
 	
 	@GetMapping("/quiz")
@@ -19,5 +19,9 @@ public class TempTestController {
 		
         return "quiz";
 	}
-	
+	@ResponseBody
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
 }
