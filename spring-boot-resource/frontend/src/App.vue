efc9939c0afc82bd2be02f2480760897e7da1f81
iuh12 <iuh12@DESKTOP-UCS6DE1>
2022-09-15 18:29:34 +0900

    upbit 빗썸 websocket으로 정보받기 끝

--- spring-boot-resource/frontend/src/App.vue
+++ spring-boot-resource/frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <MyBody msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyBody from './components/myBody/myBody'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyBody
   }
 }
 </script>
